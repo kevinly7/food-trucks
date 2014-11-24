12a84347e4d370c17a096a0ad9276a549d8b7d51
Daniel Huie <huied93@uw.edu>
2014-11-24 00:23:27 -0800

    added navigation and queue css

---
+++ js/queue.js
@@ -0,0 +1,20 @@
+"use strict";
+
+$(document).ready(function() {
+    $("#motosurf-visible").click(function(){
+        $("#motosurf").hide();
+    });
+
+    $("#hot-dawgs-visible").click(function(){
+        $("#hot-dawgs").hide();
+    });
+
+    $("#red-square-bbq-visible").click(function(){
+        $("#red-square-bbq").hide();
+    });
+
+    $("#siguanos-visible").click(function(){
+        $("#siguanos").hide();
+    });
+});
+
